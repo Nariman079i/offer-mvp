866e61f7418d2fe12b76d9ef2adf02af7d63cebc
Nariman <Nariman079i>
2023-02-23 19:53:42 +0300

    add authenticated on JWT

--- offermvp/users/urls.py
+++ offermvp/users/urls.py
@@ -1,5 +1,9 @@
 
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import *
 urlpatterns = [
@@ -7,6 +11,10 @@ urlpatterns = [
     path('user/create/', UserCreateAPI.as_view()),
     path('user/list/',UserListAPI.as_view()),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
+    path('auth/', include('djoser.urls.authtoken')),
+
+    path('jwt/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ] 
 
