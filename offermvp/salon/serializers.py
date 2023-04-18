6ef4012ad33f5296609aa99fcbecdc4b9910804a
Nariman <Nariman079i>
2023-04-19 00:37:29 +0300

    add ..

--- offermvp/salon/serializers.py
+++ offermvp/salon/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import *
 from .models import *
 
-
+class ImageSerializer(ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
 class FeedBackSerializer(ModelSerializer):
     class Meta:
         model = FeedBack
@@ -15,6 +18,7 @@ class ServicePriceSerializer(ModelSerializer):
 
 
 class ServiceSerializer(ModelSerializer):
+    images = ImageSerializer(many=True)
     class Meta:
         model = Service
         fields = "__all__"
